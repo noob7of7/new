271d6189474fc5f609859a4c0ed19aa4192ce1b4
zhan <zhan71007@gmail.com>
2025-03-19 15:07:08 +0500

    first comment

---
+++ jsflowder/script.js
@@ -0,0 +1,8 @@
+let gog = true;
+let grg = false;
+console.log (gog&&grg);
+
+let user = null;
+let defoltname = "anonim";
+let name = user??defoltname;
+console.log (name)
---
+++ script.js
@@ -0,0 +1,28 @@
+let num = 123;
+let strNum = String(num); // Использование функции String()
+console.log(strNum); // Вывод: "123"
+
+let bool = true;
+let strBool = bool.toString(); // Использование метода toString()
+console.log(strBool); // Вывод: "true"
+
+let zhan = 1263;
+let str_zhan = String(zhan);
+console.log(str_zhan);
+
+
+let str = "123";
+let num2 = Number(str); // Использование функции Number()
+console.log(num2); // Вывод: 123
+
+let intStr = "123.45";
+let intNum = parseInt(intStr); // Использование функции parseInt()
+console.log(intNum); // Вывод: 123
+console.log(intStr);
+
+let nullvalue = null;
+let undefinedvalue = undefined 
+
+console.log(String(nullvalue));
+console.log(nullvalue);
+console.log(parseInt)
